866b88539a3744902bddb972adcb0eda77a82911
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-18 17:53:43 +0930

    03-use-reach-router: nest rourtes without nested components

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 yarn add react-router-dom @reach-router
 yarn add --dev @types/react-router-dom @types/reach__router
 ```
+
+### Reach Router
+- by default: `exact`
+#### Nested Router
+- nested **component** with `childern`
+> withouth nested components, no chilredn components will be displayed
+```javascript
+const Dashboard = (props: RouteComponentProps<any>) => {
+    const { children } = props;
+
+    return (
+        <h1>Dash</h1>
+        { children }
+    )
+}
+```
+- nested **routers**
+```javascript
+<Router>
+    <Dashboard>
+          <Invoices path="invoices"></Invoices>
+          <Team path="team"></Team>
+    </Dashboard>
+</Router>
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Router, RouteComponentProps, Link, useParams } from '@reach/router';
 
-const Home = (props: RouteComponentProps<any>) => <h1>Home</h1>
+const Home = (props: RouteComponentProps<any>) => <h1>Home</h1>;
 const Dash = (props: RouteComponentProps<any>) => <h1>Dash</h1>;
+const Invoices = (props: RouteComponentProps<any>) => <h1>Invoices</h1>;
+const Team = (props: RouteComponentProps<any>) => <h1>Team</h1>;
 const Invoice = (props: RouteComponentProps<any>) => {
   const params = useParams();
   return <h1>Invoice {params.id}</h1>;
@@ -12,17 +14,18 @@ const App = () => {
   return (
     <div>
       {/* will be showed on every page */}
-      <nav> 
+      <nav>
         <Link to="/">Home</Link>
         <Link to="dashboard">Dashboard</Link>
-        <Link to="invoice/4">Invoice 4</Link>
-        <Link to="invoice/5">Invoice 5</Link>
-        <Link to="invoice/6">Invoice 6</Link>
+        <Link to="dashboard/invoices">Invoices</Link>
+        <Link to="dashboard/team">Team</Link>
       </nav>
       <Router>
         <Home path="/"></Home>
-        <Dash path="dashboard"></Dash>
-        <Invoice path="invoice/:id"></Invoice>
+        <Dash path="dashboard">
+          <Invoices path="invoices"></Invoices>
+          <Team path="team"></Team>
+        </Dash>
       </Router>
     </div>
   );
