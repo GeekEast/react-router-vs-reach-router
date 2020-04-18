f7690e1f014e1d080c450e7e1daf025625733d67
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-18 18:37:23 +0930

    03-use-reach-router: programatically navigate without hooks

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,15 @@ const Dash = (props: RouteComponentProps<any>) => (
 );
 const Dashboard = (props: RouteComponentProps<any>) => <h1>Dashboard</h1>;
 const Invoices = (props: RouteComponentProps<any>) => <h1>Invoices</h1>;
-const Team = (props: RouteComponentProps<any>) => <h1>Team</h1>;
+const Team = (props: RouteComponentProps<any>) => {
+  const { navigate } = props;
+  const onBtnClick = () => navigate!('/');
+  return (
+    <div>
+      <h1>Team</h1><button onClick={onBtnClick}>Go back to Home</button>
+    </div>
+  );
+};
 const HomeNav = (props: RouteComponentProps<any>) => <h1>Home Navigation</h1>;
 const DashboardNav = (props: RouteComponentProps<any>) => (
   <h1>Dashboard Navigation</h1>
